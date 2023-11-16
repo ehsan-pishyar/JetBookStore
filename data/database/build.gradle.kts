ae9f76bfc836c2896d2b466b0553594eef01d0ab
Ehsan Pishyar <ehsan.pishyar@gmail.com>
2023-11-17 01:42:16 +0330

    Database Branch
    1. Added data:database module to project to store book objects which we fetch them from webservice to that for Single Source of Truth purpose
    2. Added dependencies to database module
    - Room: Room database that is an abstract class of SQLite for creating our database to store Book objects
    - Dagger Hilt: for Singleton pattern purpose to create only one instance of database classes
    - Gson Converter: to convert json to kotlin data objects
    3. Added packages to database module
    - converters
    - models
    - di
    - dao
    - utils

---
+++ data/database/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.example.database"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_18.toString()
+    }
+}
+
+dependencies {
+
+    val androidCoreVersion = "1.10.1"
+    val roomVersion = "2.5.1"
+    val hiltVersion = "2.45"
+    val retrofitVersion = "2.9.0"
+
+    implementation("androidx.core:core-ktx:$androidCoreVersion")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Room
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    // Gson converter
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    // Dagger - Hilt
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+}
